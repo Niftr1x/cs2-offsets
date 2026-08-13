eb50461810b4d14d9639c2cc3dea47a0743f5a2c
Stanley <entitycze@gmail.com>
2026-08-13 12:18:50 +0200

    Fresh ones updated for latest build

--- host_dll.rs
+++ host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-12 23:13:23.529796100 UTC
+// 2026-08-13 10:17:30.522927100 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- steamaudio_dll.hpp
+++ steamaudio_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-12 23:13:23.529796100 UTC
+// 2026-08-13 10:17:30.522927100 UTC
 
 #pragma once
 
